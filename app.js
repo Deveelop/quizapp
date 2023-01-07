31bf829935bf2e49967bfee568b984140d743547
vicson <vicsonabuka@gmail.com>
2023-01-07 04:07:41 -1200

    Add array of questions

---
+++ app.js
@@ -0,0 +1,76 @@
+const quizData = [{
+    subjects: 'Mathematics',
+    questions:[
+        {
+        question:'what is 1+1 ?',
+        options:[2,4,6,8],
+        correctOption:2
+    },
+    {
+        question: 'what is 20/2 ?',
+        options:[10.2,10,5,200],
+        correctOption:10
+    },
+    {
+        question: 'what is 20*2 ?',
+        options:[200,100,40,20],
+        correctOption:40
+    },
+    {
+        question: 'what is a square ?',
+        options:['two dimentional shape','equal shape','having length and breadth equal','4 equal sides and 4 vertices'],
+        correctOption:'4 equal sides and 4 vertices'
+    }
+]
+},
+{
+    subject: 'English',
+    questions:[
+        {
+            question: '___ is a name of person, animal, place or things.',
+            options: ['pronoun', 'noun', 'verb', 'action'],
+            correctOption: 'noun'
+        },
+        {
+            question: '___ action word.',
+            options: ['noun', 'pronoun', 'verb', 'school'],
+            correctOption: 'verb'
+        },
+        {
+            question: '___ is used instead of a noun.',
+            options: ['proonoun', 'pronoun', 'adverb', 'none of the above'],
+            correctOption: 'pronoun'
+        },
+        {
+            question: 'a young dog is called ?',
+            options: ['little dog', 'small dog', 'poppi', 'puppy'],
+            correctOption: 'puppy'
+        }
+    ]
+},
+{
+    subject: 'Coding',
+    questions: [
+        {
+            question: 'HTML stands for ?',
+            options:['hipertext makeup language', 'hypertext mark language', 'highertext markup language', 'hypertext markup language'],
+            correctOption: 'hypertext markup language'
+        },
+        {
+            question: 'CSS stands for ?',
+            options:['cascade styling sheet', 'cascading styles', 'cascading style sheet', 'casting styles sheet'],
+            correctOption: 'cascading style sheet'
+        },
+        {
+            question: 'JS stands for ?',
+            options:['java script', 'javascript', 'javascrypt', 'javaskip'],
+            correctOption: 'javascript'
+        },
+        {
+            question: 'what HTML tag is used for linking JS files ?',
+            options:['<script>', '<link>', '<root>', '<j>'],
+            correctOption: '<script>'
+        }
+    ]
+}
+]
